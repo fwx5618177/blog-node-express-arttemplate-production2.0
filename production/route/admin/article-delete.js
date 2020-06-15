c13b36124d618f3a2fd9c53881385e2a82496cca
fwx-gitrepo3-test5 <fwx5618177@gmail.com>
2020-06-16 01:31:48 +0900

    add del uploads files

--- production/route/admin/article-delete.js
+++ production/route/admin/article-delete.js
@@ -1,8 +1,25 @@
 const { Article } = require('../../model/article');
+const path = require('path');
+const fs = require('fs');
 
 module.exports = async (req, res) => {
     const deleId = req.query.id;
     // return res.send(deleId);
+    
+
+    const article = await Article.findOne({_id: deleId});
+    // console.log(article);
+
+    // return res.send(article.cover);
+    const cover = article.cover;
+    const uploadDir = path.join(__dirname, '../../public');
+    fs.unlink(`${uploadDir}/${cover}`,(err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log('delete ok');
+        }
+      });
 
     await Article.findOneAndDelete({_id: deleId});
     return res.redirect('/admin/article');
